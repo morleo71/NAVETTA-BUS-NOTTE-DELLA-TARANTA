8744fb2555160b8a405ac35990d9638e40a6bd6d
morleo71 <mattiamorleo71@gmail.com>
2025-07-04 09:41:11 +0200

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,25 @@
+const cacheName = 'ordine-form-v1';
+const assets = [
+  './',
+  './index.html',
+  './manifest.json',
+  './service-worker.js',
+  './icons/icon-192.png',
+  './icons/icon-512.png'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(cacheName).then(cache => {
+      return cache.addAll(assets);
+    })
+  );
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(res => {
+      return res || fetch(e.request);
+    })
+  );
+});
